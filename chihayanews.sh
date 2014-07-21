423212f74a08ed11709359bc719a491e2413145f
Yuma Arakawa <yuarkw@gmail.com>
2014-07-21 12:55:14 +0900

    cache_dirに指定したディレクトリに一時ファイルを保存するよう修正
    cache_dirはデフォルトでは"~/.chihayanews.d"

--- chihayanews.sh
+++ chihayanews.sh
@@ -9,6 +9,8 @@
 # http://nanoway.net/web/nicovideo-comment-api
 # http://dic.nicovideo.jp/a/ニコニコ動画api
 
+cache_dir=$(echo ~/.chihayanews.d)
+
 usage() {
 	echo "Usage: $(basename $0) [-l|--login]"
 }
@@ -20,35 +22,42 @@ login() {
 	if [ -z "$SH_CHYNS_PW" ]; then
 		SH_CHYNS_PW=$(sed -n 2p ~/.chihayanews)
 	fi
-	wget --post-data "mail=${SH_CHYNS_MAIL}&password=${SH_CHYNS_PW}" -q -O - --save-cookies=cookie1.txt --keep-session-cookies https://secure.nicovideo.jp/secure/login?site=niconico > /dev/null
-	wget --save-cookies=cookie2.txt --keep-session-cookies -q -O - --load-cookies=cookie1.txt http://flapi.nicovideo.jp/api/getflv/sm4538955 | tr = @ | tr % = | nkf -WwmQ | tr @ = | sed 's/&/\n/g' > decoded.txt
+	wget --post-data "mail=${SH_CHYNS_MAIL}&password=${SH_CHYNS_PW}" -q -O - --save-cookies=${cache_dir}/cookie1.txt --keep-session-cookies https://secure.nicovideo.jp/secure/login?site=niconico > /dev/null
+	wget --save-cookies=${cache_dir}/cookie2.txt --keep-session-cookies -q -O - --load-cookies=${cache_dir}/cookie1.txt http://flapi.nicovideo.jp/api/getflv/sm4538955 | tr = @ | tr % = | nkf -WwmQ | tr @ = | sed 's/&/\n/g' > ${cache_dir}/decoded.txt
 }
 
 main() {
-	if [ ! -f decoded.txt ]; then
+	if [ ! -f ${cache_dir}/decoded.txt ]; then
 		login
 	fi
 
-	thread_id=$(grep '^thread_id=' decoded.txt | cut -d'=' -f2)
-	ms=$(grep '^ms=' decoded.txt | cut -d'=' -f2)
-	user_id=$(grep '^user_id=' decoded.txt | cut -d'=' -f2)
+	thread_id=$(grep '^thread_id=' ${cache_dir}/decoded.txt | cut -d'=' -f2)
+	ms=$(grep '^ms=' ${cache_dir}/decoded.txt | cut -d'=' -f2)
+	user_id=$(grep '^user_id=' ${cache_dir}/decoded.txt | cut -d'=' -f2)
 
 	## threadkey=""(空) と force_184="1" は、変わらないようなので
 	## とりあえず、getthreadkeyの取得とパースは行わない
-	# wget --save-cookies=cookie3.txt --keep-session-cookies -q -O - --load-cookies=cookie1.txt http://flapi.nicovideo.jp/api/getthreadkey?thread=${thread_id} > getthreadkey.html
+	# wget --save-cookies=${cache_dir}/cookie3.txt --keep-session-cookies -q -O - --load-cookies=${cache_dir}/cookie1.txt http://flapi.nicovideo.jp/api/getthreadkey?thread=${thread_id} > ${cache_dir}/getthreadkey.html
 
-	wget --post-data "<thread thread=\"${thread_id}\" version=\"20090904\" res_from=\"-1000\" scores=\"1\" nicoru=\"1\" threadkey=\"\" force_184=\"1\" user_id=\"${user_id}\" />" -q -O - --save-cookies=cookie1.txt --keep-session-cookies ${ms} > ms.html
+	wget --post-data "<thread thread=\"${thread_id}\" version=\"20090904\" res_from=\"-1000\" scores=\"1\" nicoru=\"1\" threadkey=\"\" force_184=\"1\" user_id=\"${user_id}\" />" -q -O - --save-cookies=${cache_dir}/cookie1.txt --keep-session-cookies ${ms} > ${cache_dir}/ms.html
 
-	sed 's/<\/chat>/\n/g' ms.html | sed 's/<chat /\n/g' | sed 's/>/ /g' | sed '/^ *$/d' | tail -n +2 | head -n -1 > notag.txt
+	sed 's/<\/chat>/\n/g' ${cache_dir}/ms.html | sed 's/<chat /\n/g' | sed 's/>/ /g' | sed '/^ *$/d' | tail -n +2 | head -n -1 > ${cache_dir}/notag.txt
 
-	awk 'BEGIN{FS=" ";OFS=","}{print $3,$NF}' notag.txt | cut -c7- | tr -d '"' | awk 'BEGIN{FS=","}{if($1 < 2000){print $2}}' > chihayanews.txt
+	awk 'BEGIN{FS=" ";OFS=","}{print $3,$NF}' ${cache_dir}/notag.txt | cut -c7- | tr -d '"' | awk 'BEGIN{FS=","}{if($1 < 2000){print $2}}' > ${cache_dir}/chihayanews.txt
 
-	tail -n 1 chihayanews.txt
+	tail -n 1 ${cache_dir}/chihayanews.txt
 }
 
 if [ $# -gt 1 ]; then
 	usage
 	exit 1
+fi
+if [ -n ${cache_dir} ]; then
+	if [ ! -d ${cache_dir} ]; then
+		mkdir ${cache_dir}
+	fi
+else
+	cache_dir=''
 fi
 if [ -n "$1" ]; then
 	case "$1" in
